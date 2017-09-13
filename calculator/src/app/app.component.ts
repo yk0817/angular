9ae9a8686120582a4470301e7658d349950d017c
山本憲 <em072029@gmail.com>
2017-09-13 23:28:39 +0900

    数値入力です。

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -9,10 +9,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'calculator';
   result = 'result';
+  msg = 'test';
+  test = '---';
+  flag = "test";
   nav_numbers = ['7','8','9','4','5','6','1','2','3','0','00','.'];
   nav_operators = ['/','*','-','+','='];
   nav_erases = ["CE","CA","DEL"];
-  show_click(e: any){
+  show_click(){
+    this.title = "click!"
+    this.flag = "true"
     console.log(this);
   }
+  new_flag(e){
+    console.log(e);
+  }
+  
 }
