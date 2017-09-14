972835785d6da1ae41b8a8a317f6cbb5c507c34f
山本憲 <em072029@gmail.com>
2017-09-14 21:33:23 +0900

    流れを書き下す。

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -9,19 +9,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'calculator';
   result = 'result';
-  msg = 'test';
-  test = '---';
-  flag = "test";
+  is_operate = false; //演算子でない場合 false
+
+  total_num = 0;
+  input_num = 0; 
+  my_calc = "";
+  
   nav_numbers = ['7','8','9','4','5','6','1','2','3','0','00','.'];
   nav_operators = ['/','*','-','+','='];
-  nav_erases = ["CE","CA","DEL"];
-  show_click(){
-    this.title = "click!"
-    this.flag = "true"
-    console.log(this);
+  nav_erases = ["C","C","C"];
+  
+  show_click(e){
+    if (this.is_operate) {
+    } if (Number(this.input_num) == 0) {
+      this.input_num += e;
+    }
+    else {
+      console.log(Number(this.input_num));
+      this.input_num += e;
+    }
   }
-  new_flag(e){
-    console.log(e);
+  clear_num(e){
+    this.input_num = 0;
+  }
+  
+  
+  display_num(e){
+    //console.log(typeof(this.result)); 
+    this.result = e;
+  }
+  
+  evaluate_arithmetic_operation(e){
+    //式を計算
+    
+  }
+  
+  evaluate_alphabet(e){
+    //アルファベットを評価する。
+    
   }
   
 }
